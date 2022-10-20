e22f23add8ace7181a2a05bbb7d58c90dc9e13f8
prawstho <peter.rawsthorne@keyin.com>
2022-10-20 08:49:50 -0230

    Added display config

--- config.js
+++ config.js
@@ -5,13 +5,25 @@ const logEvents = require('./logEvents');
 // define/extend an EventEmitter class
 const EventEmitter = require('events');
 class MyEmitter extends EventEmitter {};
-
 // initialize an new emitter object
 const myEmitter = new MyEmitter();
 // add the listener for the logEvent
 myEmitter.on('log', (event, level, msg) => logEvents(event, level, msg));
 
+// Node.js common core global modules
+const fs = require('fs');
+const fsPromises = require('fs').promises;
+const path = require('path');
 const myArgs = process.argv.slice(2);
+
+function displayConfig() {
+    if(DEBUG) console.log('config.displayConfig()');
+    fs.readFile(__dirname + "/json/config.json", (error, data) => {
+        if(error) throw error;         
+        console.log(JSON.parse(data));
+    });
+    myEmitter.emit('log', 'config.displayConfig()', 'INFO', 'config.json displayed');
+}
 
 function configApp() {
     if(DEBUG) console.log('configApp()');
@@ -20,7 +32,7 @@ function configApp() {
     switch (myArgs[1]) {
     case '--show':
         if(DEBUG) console.log('--show');
-        //displayConfig();
+        displayConfig();
         break;
     case '--reset':
         if(DEBUG) console.log('--reset');
